d8ecec97bcab3edb4d07acc7011dc317e8d3fe6d
Chap Ambrose <cambrose@salesforce.com>
2021-08-04 16:55:17 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -114,3 +114,34 @@ resource "kubernetes_service" "nginx" {
 output "lb_ip" {
   value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname
 }
+
+
+resource "kubernetes_cron_job" "demo" {
+  metadata {
+    name = "demo"
+  }
+  spec {
+    concurrency_policy            = "Replace"
+    failed_jobs_history_limit     = 5
+    schedule                      = "1 0 * * *"
+    starting_deadline_seconds     = 10
+    successful_jobs_history_limit = 10
+    job_template {
+      metadata {}
+      spec {
+        backoff_limit              = 2
+        ttl_seconds_after_finished = 10
+        template {
+          metadata {}
+          spec {
+            container {
+              name    = "hello"
+              image   = "busybox"
+              command = ["/bin/sh", "-c", "date; echo Hello from the Kubernetes cluster via cron"]
+            }
+          }
+        }
+      }
+    }
+  }
+}
