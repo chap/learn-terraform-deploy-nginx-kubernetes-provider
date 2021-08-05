721fcd7a6667da1d224320491a4f4be3fc420a0d
Chap Ambrose <cambrose@salesforce.com>
2021-08-05 17:07:44 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -227,19 +227,10 @@ resource "aws_route53_record" "cert_validation-1" {
   ttl      = 60
 }
 
-resource "aws_route53_record" "cert_validation-2" {
-  name     = aws_acm_certificate.cert.domain_validation_options.2.resource_record_name
-  type     = aws_acm_certificate.cert.domain_validation_options.2.resource_record_type
-  zone_id  = var.hosted-zone
-  records  = [aws_acm_certificate.cert.domain_validation_options.2.resource_record_value]
-  ttl      = 60
-}
-
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [
     aws_route53_record.cert_validation-0.fqdn,
     aws_route53_record.cert_validation-1.fqdn,
-    aws_route53_record.cert_validation-2.fqdn
   ]
 }
