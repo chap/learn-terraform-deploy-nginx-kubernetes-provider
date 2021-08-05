615c9ffc286a9d7f0a80345cf585a9a8cf6e261b
Chap Ambrose <cambrose@salesforce.com>
2021-08-05 17:16:38 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -204,7 +204,7 @@ JSON
 
 
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "example.com"
+  domain_name       = "53.chapambrose.com"
   validation_method = "DNS"
   
   lifecycle {
@@ -212,14 +212,10 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-data "aws_route53_zone" "example" {
-  name         = "example.com"
-  private_zone = false
-}
 
 resource "aws_route53_record" "example" {
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -235,7 +231,7 @@ resource "aws_route53_record" "example" {
 }
 
 resource "aws_acm_certificate_validation" "example" {
-  certificate_arn         = aws_acm_certificate.example.arn
+  certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
 }
 
