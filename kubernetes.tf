2790df5a3454f57ef67fffb0526289a4ef8a3672
Chap Ambrose <cambrose@salesforce.com>
2021-08-05 17:13:48 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -202,35 +202,45 @@ JSON
   }
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name               = "53.chapambrose.com"
-  validation_method         = "DNS"
 
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "example.com"
+  validation_method = "DNS"
+  
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_route53_record" "cert_validation-0" {
-  name     = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type     = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  zone_id  = var.hosted-zone
-  records  = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
-  ttl      = 60
+data "aws_route53_zone" "example" {
+  name         = "example.com"
+  private_zone = false
 }
 
-resource "aws_route53_record" "cert_validation-1" {
-  name     = aws_acm_certificate.cert.domain_validation_options.1.resource_record_name
-  type     = aws_acm_certificate.cert.domain_validation_options.1.resource_record_type
-  zone_id  = var.hosted-zone
-  records  = [aws_acm_certificate.cert.domain_validation_options.1.resource_record_value]
-  ttl      = 60
+resource "aws_route53_record" "example" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
 }
 
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [
-    aws_route53_record.cert_validation-0.fqdn,
-    aws_route53_record.cert_validation-1.fqdn,
-  ]
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+}
+
+resource "aws_lb_listener" "example" {
+  # ... other configuration ...
+
+  certificate_arn = aws_acm_certificate_validation.example.certificate_arn
 }
